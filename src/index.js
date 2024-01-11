1dcd3c34c9e7e2d288e90673d7cd7cb82802cb0d
Shiv <vihsshukla@gmail.com>
2024-01-11 20:16:12 +0530

    Enabled CORS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express=require('express');
 const path=require('path');
+const cors=require('cors');
 
 const app=express();
 
 const port=process.env.PORT;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.disable('etag');
 
